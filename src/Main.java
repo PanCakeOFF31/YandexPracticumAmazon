b290cf2a23fb5ebfe9c188ce3b26a6349422e461
PanCakeOFF <mak.blinov.original@yandex.ru>
2023-09-14 21:18:56 +0300

    Просто проверка, поставил комментарий.

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
 
 //      Взаимное рецензирование
-        peerReview();
+//        peerReview();
 
 //        Самостоятельная работа
 //        sprints();
